ddc571d9f11a8ef33636c99937b1c1932e6c1df5
Vladimir Kovtunovskiy <vladimir.vvk93@gmail.com>
2015-11-14 11:08:05 +0300

    Started week 5 task

---
+++ AlgTrains/Algorithms/Week5/DijkstraShortestPath.cs
@@ -0,0 +1,27 @@
+﻿using AlgTrains.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgTrains.Algorithms.Week5
+{
+    public class DijkstraShortestPath : ITaskPerformer
+    {
+        public string TaskDescription
+        {
+            get { return "Task 5: Computing Dijkstra's Shortest Path"; }
+        }
+
+        public string FileName
+        {
+            get { return "dijkstraData.txt"; }
+        }
+
+        public Task PerformTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
